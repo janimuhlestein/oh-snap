d6dbf35ce6e2260a7c9b2355fb42f07cc6ab0d3d
Jani Muhlestein <janimuhlestein@gmail.com>
2020-12-12 16:38:02 -0700

    about tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+//cleanup after each test
+afterEach(cleanup);
+
+describe('About component', ()=> {
+    //renders About test
+    //first test
+    it('renders', () => {
+        render(<About />);
+    });
+
+    //second test
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+});
