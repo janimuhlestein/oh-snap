77c7419ff7878d980af0f4c31110c12be9cf98b4
Jani Muhlestein <janimuhlestein@gmail.com>
2020-12-14 10:02:44 -0700

    minor changes to nav

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+//import { capitalizeFirstLetter } from '../../utils/helpers';
 import photo from "../../assets/small/commercial/0.jpg";
 
 function Gallery(props) {
@@ -10,7 +10,7 @@ function Gallery(props) {
     };
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <h1>{currentCategory.name}</h1>
             <p>{currentCategory.name}</p>
             <div>
                 <img src= {photo}
