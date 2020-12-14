872c70a6c285750380cd428922a46317d041998c
Jani Muhlestein <janimuhlestein@gmail.com>
2020-12-14 12:29:04 -0700

    removed useEffect

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
@@ -7,10 +7,6 @@ function Nav(props) {
     setCurrentCategory,
     currentCategory,
   } = props;
-  
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
