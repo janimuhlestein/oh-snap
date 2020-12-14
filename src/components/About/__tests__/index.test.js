01536b29047b01d336e393aa4879036802ae8293
Jani Muhlestein <janimuhlestein@gmail.com>
2020-12-14 12:13:10 -0700

    added tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -3,6 +3,12 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+];
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 //cleanup after each test
 afterEach(cleanup);
 
@@ -10,7 +16,11 @@ describe('About component', ()=> {
     //renders About test
     //first test
     it('renders', () => {
-        render(<About />);
+        render(<About 
+            categories = {categories}
+            setCurrentCategory = {mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
     });
 
     //second test
